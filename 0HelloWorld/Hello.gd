6cc13575b77d57451615bf2ca3f033d61c515507
mscoder <mscoder@hotmail.com>
2019-04-16 12:59:52 +0800

    后台代码创建node

--- 0HelloWorld/Hello.gd
+++ 0HelloWorld/Hello.gd
@@ -4,9 +4,13 @@ extends Panel
 # var a = 2
 # var b = "text"
 
+var s
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_node("Button").connect("pressed",self,"_on_Button_pressed")
+	s = Label.new() # Create a new sprite!
+	s.text="从代码创建"
+	add_child(s) # Add it as a child of this node.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -15,3 +19,9 @@ func _ready():
 
 func _on_Button_pressed():
 	get_node("Button").text="Hello World"
+
+var accum=0
+
+func _process(delta):
+	accum+=delta
+	get_node("Label").text=str(accum)
